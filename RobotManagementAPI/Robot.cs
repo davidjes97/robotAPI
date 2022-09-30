216dbd2d5af52a59e49f7b997b437709c1a9996c
jdavidOST <77995472+jdavidOST@users.noreply.github.com>
2022-09-29 21:22:25 -0400

    restructure

---
+++ RobotManagementAPI/Robot.cs
@@ -0,0 +1,8 @@
+﻿//Seperated class for API
+public class Robot
+{
+    public string RobotId { get; set; }
+    public int BatteryLevel { get; set; }
+    public int y { get; set; }
+    public int x { get; set; }
+}
--- robotAPI/RobotManagementAPI/Robot.cs
+++
@@ -1,8 +0,0 @@
-﻿//Seperated class for API
-public class Robot
-{
-    public string RobotId { get; set; }
-    public int BatteryLevel { get; set; }
-    public int y { get; set; }
-    public int x { get; set; }
-}
